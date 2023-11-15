0b70743866db8877c5aa00986508a48a06dcb64e
Aadarsh <71810927+dotAadarsh@users.noreply.github.com>
2023-11-16 04:14:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import pangea.exceptions as pe
 from pangea.config import PangeaConfig
 from pangea.services import Redact
 from pangea.services import UrlIntel
+from pangea.services import DomainIntel
 import re
 import json
 
@@ -11,6 +12,16 @@ token = "your_pangea_token"
 domain = "aws.us.pangea.cloud"
 config = PangeaConfig(domain=domain)
 
+
+def find_domain(text):
+    domain_regex = r"(?:[a-z0-9](?:[a-z0-9\-]{0,61}[a-z0-9])?\.)+[a-z]{2,6}"
+    match = re.search(domain_regex, text)
+    if match:
+        domain = match.group(0)
+        return domain
+    else:
+        return None
+        
 # Redaction function using Pangea Cloud
 def go_redact(text):
     redact = Redact(token, config=config)
@@ -18,12 +29,12 @@ def go_redact(text):
     try:
         redact_response = redact.redact(text=text, rulesets=["SECRETS"])
         print(f"Redacted text: {redact_response.result.redacted_text}")
-
+        
         # Check if the original text was redacted
         if text == redact_response.result.redacted_text:
             return ""
         return redact_response.result.redacted_text
-
+    
     except pe.PangeaAPIException as e:
         print(f"Embargo Request Error: {e.response.summary}")
         for err in e.errors:
@@ -33,7 +44,7 @@ def go_redact(text):
 # URL intelligence function using Pangea Cloud
 def go_url_intel(url):
     intel = UrlIntel(token, config=config)
-
+    
     try:
         response = intel.reputation(
             url=url,
@@ -51,26 +62,47 @@ def go_url_intel(url):
             print(f"\t{err.detail} \n")
             return err.detail
 
+
+def go_whois(domain):
+    intel = DomainIntel(token, config=config)
+    print("Checking domain...")
+
+    try:
+        response = intel.who_is(domain=domain, provider="whoisxml", verbose=True, raw=True)
+        print(f"Response: {response.result.data}")
+        domain_name = response.result.data.domain_name
+        domain_availability = response.result.data.domain_availability
+        created_date = response.result.data.created_date
+        registrar_name = response.result.data.registrar_name
+        registrant_organization = response.result.data.registrant_organization
+        return [domain_name, domain_availability, created_date, registrar_name, registrant_organization]
+
+    except pe.PangeaAPIException as e:
+        print(f"Request Error: {e.response.summary}")
+        for err in e.errors:
+            print(f"\t{err.detail} \n")
+            return None
+
 # Discord client class
 class MyClient(discord.Client):
-
+    
     async def on_ready(self):
         # Event handler when the bot is ready and connected
         print(f'Logged on as {self.user}!')
-
+    
     async def on_message(self, message):
         # Event handler for incoming messages
-
+        
         # Print the details of the incoming message
         print(f'Message from {message.author}: {message.content}')
-
+        
         # Ignore messages from the bot itself
         if message.author == self.user:
             return
-
+        
         # Redact sensitive information in the message content
         redacted_msg = go_redact(message.content)
-
+        
         # Check if redaction resulted in any changes
         if redacted_msg != "":
             # Delete the original message
@@ -79,10 +111,10 @@ class MyClient(discord.Client):
             await message.channel.send("API Key found!")
             # Send the redacted message
             await message.channel.send(redacted_msg)
-
+        
         # Search for URLs in the message content
         match = re.search(r'(http|https)://(?P<hostname>[a-zA-Z0-9-]{1,63}(?:\.[a-zA-Z0-9-]{1,63})*)[^\s]+', message.content)
-
+        
         if match:
             # Extract the detected URL
             detected_url = match.group()
@@ -93,6 +125,25 @@ class MyClient(discord.Client):
                 # If the URL is deemed malicious, send a warning to the channel
                 if url_verdict == "malicious":
                     await message.channel.send("Malicious website found!",  reference=message)
+
+        if message.content.startswith('!whois'):
+            input_domain = find_domain(message.content)
+            if input_domain is not None:
+                whois_response = go_whois(input_domain)
+                if whois_response is not None:
+                    domain_name, domain_availability, created_date, registrar_name, registrant_organization = whois_response
+
+                    embed = discord.Embed(title="Whois", description="This is an important announcement for all members.", color=discord.Color.blue())
+                    embed.add_field(name="Domain Name", value=domain_name)
+                    embed.add_field(name="Domain Availability", value=domain_availability)
+                    embed.add_field(name="Created Date", value=created_date)
+                    embed.add_field(name="Registrar Name", value=registrar_name)
+                    embed.add_field(name="Registrant Organization", value=registrant_organization)
+
+                
+                await message.reply(embed=embed, mention_author=True)
+                
+
 
 # Set up Discord intents
 intents = discord.Intents.default()
